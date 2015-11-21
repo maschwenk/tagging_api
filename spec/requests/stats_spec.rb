d8215973c0d7f9be2299a7e99e88d2f5b08820ed
Max Schwenk <maschwenk@gmail.com>
2015-11-20 22:10:29 -0500

    Fill out stats tests

--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -1,14 +1,15 @@
 describe "Stats API" do
-  describe "GET /stats " do
+  describe "GET /api/stats " do
     it "Retrieves statistics about all tags" do
-
+      get "/api/stats/", {}, { "Accept" => "application/json" }
     end
   end
-  describe "GET /stats/:entity_type/:entity_id" do
+  describe "GET /api/stats/:entity_type/:entity_id" do
     #my logic for this route is similar, doesn't make sense for this to be stats route
     #given these stats are speicific to a given entity
     it "Retrieves statistics about a specific tagged entity" do
-
+      FactoryGirl.create :product, name: ""
+      get "/api/products/1/", {}, { "Accept" => "application/json" }
     end
   end
 
