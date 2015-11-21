af1c9f4c11ec85369e8bf69d0ce0659069cbbf3d
Max Schwenk <maschwenk@gmail.com>
2015-11-20 19:08:38 -0500

    Give us a simple entity to test against

---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+quantity 1
+  end
+
+end
--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -7,7 +7,7 @@ describe "Stats API" do
   describe "GET /stats/:entity_type/:entity_id" do
     #my logic for this route is similar, doesn't make sense for this to be stats route
     #given these stats are speicific to a given entity
-    it "Retrives statistics about a specific tagged entity" do
+    it "Retrieves statistics about a specific tagged entity" do
 
     end
   end
