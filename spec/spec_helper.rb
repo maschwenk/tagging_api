f0b01f7287a14ce92aeae5366d5f53e6d0e4a21b
Max Schwenk <maschwenk@gmail.com>
2015-11-20 21:32:46 -0500

    Fix the config of this for FactoryGirl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,12 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'rails_helper'
+require "factory_girl"
+
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -30,6 +35,7 @@ RSpec.configure do |config|
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
