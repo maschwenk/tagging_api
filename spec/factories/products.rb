56a45978b2882cba6596382e5fea26a70aa8bccd
Max Schwenk <maschwenk@gmail.com>
2015-11-20 19:30:39 -0500

    Create some simple factory for the products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
+
+  sequence :product_name do |n|
+    "Mission Impossible #{n} (DVD)"
+  end
   factory :product do
-    name "MyString"
+    name { generate(:product_name) }
     quantity 1
   end
 
